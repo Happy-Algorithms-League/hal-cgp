853c03604e684c967bb6ea1c0a840fdb7b2d03b5
Jakob Jordan <jakobjordan@posteo.de>
2018-08-16 22:38:01 +0200

    Add test for CGPAdd/Sub

--- test/test_cgp.py
+++ test/test_cgp.py
@@ -6,6 +6,44 @@ sys.path.insert(0, '../')
 import gp
 
 
+def test_add():
+    params = {
+        'n_inputs': 2,
+        'n_outputs': 1,
+        'n_columns': 1,
+        'n_rows': 1,
+    }
+
+    primitives = gp.CGPPrimitives([gp.CGPAdd])
+    genome = gp.CGPGenome(params['n_inputs'], params['n_outputs'], params['n_columns'], params['n_rows'], primitives)
+    genome.dna = [0, -2, -1, 0]
+    graph = gp.CGPGraph(genome, primitives)
+
+    x = [5., 1.5]
+    y = graph(x)
+
+    assert(abs(x[0] + x[1] - y[0]) < 1e-15)
+
+
+def test_sub():
+    params = {
+        'n_inputs': 2,
+        'n_outputs': 1,
+        'n_columns': 1,
+        'n_rows': 1,
+    }
+
+    primitives = gp.CGPPrimitives([gp.CGPSub])
+    genome = gp.CGPGenome(params['n_inputs'], params['n_outputs'], params['n_columns'], params['n_rows'], primitives)
+    genome.dna = [0, -2, -1, 0]
+    graph = gp.CGPGraph(genome, primitives)
+
+    x = [5., 1.5]
+    y = graph(x)
+
+    assert(abs(x[0] - x[1] - y[0]) < 1e-15)
+
+
 def test_cgp():
     params = {
         'seed': 1234,
