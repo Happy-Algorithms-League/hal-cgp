254f6efc7ddc36ece2f4c5a6d13fac4e782fd5ea
Jakob Jordan <jakobjordan@posteo.de>
2018-12-06 13:04:30 +0100

    Update evo regression example

--- examples/example_evo_regression.py
+++ examples/example_evo_regression.py
@@ -4,23 +4,57 @@ import scipy.constants
 import sympy
 from sympy.printing.dot import dotprint
 import sys
+import time
 import torch
 
 sys.path.insert(0, '../')
 import gp
 
 
+def objective(individual):
+
+    if individual.fitness is not None:
+        return individual
+
+    n_function_evaluations = 1000
+
+    graph = gp.CGPGraph(individual.genome)
+    f_graph = graph.compile_torch_class()
+
+    def f_target(x):  # target function
+        # return 2.7182 + x[:, 0] - x[:, 1]
+        # return (1. + x[:, 0]) * (1. + x[:, 1])
+        return 1. / (1. + x[:, 0]) + 1. / (1. + x[:, 1])
+        # return 1. / (1. + 1. / x[:, 0]) + 1. / (1. + 1. / x[:, 1])
+        # return 1. / (1. + 1. / x[:, 0] ** 2) + 1.
+        # return 1. / (1. + 1. / x[:, 0] ** 2) + 1. / (1. + 1. / x[:, 1] ** 2)  # solved, ~50k
+        # return 1. / (1. + 1. / x[:, 0] ** 4) + 1. / (1. + 1. / x[:, 1] ** 4)
+        # return 1. / (1. + 1. / x[:, 0] ** 2)
+        # return x[:, 0] ** 2 + 2 * x[:, 0] * x[:, 1] + x[:, 1] ** 2  # solved, ~3k
+
+    x = torch.Tensor(n_function_evaluations, 2).uniform_(-5, 5)
+    x[torch.abs(x) < 1e-5] = 1.  # avoid infinities due to zero division
+    y = f_graph(x)
+
+    loss = torch.mean((f_target(x) - y[:, 0]) ** 2)
+
+    individual.fitness = -loss.item()
+
+    return individual
+
+
 def evo_regression():
     params = {
         'seed': 8188212,
+        'n_threads': 1,
 
         # evo parameters
         'n_parents': 1,
         'n_offspring': 4,
-        'max_generations': 200000,
+        'max_generations': 1000,
         'min_fitness': 0.,
         'n_breeding': 4,
-        'tournament_size': 2,
+        'tournament_size': 1,
         'mutation_rate': 0.02,
 
         # cgp parameters
@@ -36,48 +70,12 @@ def evo_regression():
 
     primitives = gp.CGPPrimitives([gp.CGPAdd, gp.CGPSub, gp.CGPMul, gp.CGPDiv, gp.CGPConstantFloat])
 
-    n_evaluations = 0
-
-    def objective(genome):
-
-        nonlocal n_evaluations
-        n_evaluations += 1
-
-        torch.manual_seed(params['seed'])
-
-        n_function_evaluations = 1000
-
-        graph = gp.CGPGraph(genome)
-        f_graph = graph.compile_torch_class()
-
-        def f_target(x):  # target function
-            # return 2.7182 + x[:, 0] - x[:, 1]
-            # return (1. + x[:, 0]) * (1. + x[:, 1])
-            # return 1. / (1. + x[:, 0]) + 1. / (1. + x[:, 1])
-            # return 1. / (1. + 1. / x[:, 0]) + 1. / (1. + 1. / x[:, 1])
-            # return 1. / (1. + 1. / x[:, 0] ** 2) + 1.
-            # return 1. / (1. + 1. / x[:, 0] ** 2) + 1. / (1. + 1. / x[:, 1] ** 2)  # solved, ~50k
-            return 1. / (1. + 1. / x[:, 0] ** 4) + 1. / (1. + 1. / x[:, 1] ** 4)
-            # return 1. / (1. + 1. / x[:, 0] ** 2)
-            # return x[:, 0] ** 2 + 2 * x[:, 0] * x[:, 1] + x[:, 1] ** 2  # solved, ~3k
-
-        # x = torch.Tensor(n_function_evaluations, params['n_inputs']).normal_()
-        x = torch.Tensor(n_function_evaluations, params['n_inputs']).uniform_(-5, 5)
-        x[torch.abs(x) < 1e-5] = 1.  # avoid infinities due to zero division
-        y = f_graph(x)
-
-        loss = torch.mean((f_target(x) - y[:, 0]) ** 2)
-
-        return -loss.item()
-
     # create population object that will be evolved
     pop = gp.CGPPopulation(
         params['n_parents'], params['n_offspring'], params['n_breeding'], params['tournament_size'], params['mutation_rate'],
-        params['n_inputs'], params['n_outputs'], params['n_columns'], params['n_rows'], params['levels_back'], primitives)
+        params['n_inputs'], params['n_outputs'], params['n_columns'], params['n_rows'], params['levels_back'], primitives, n_threads=params['n_threads'])
 
     history_fitness, history_average_phenotype_distance, history_average_genotype_distance = pop.evolve(objective, params['max_generations'], params['min_fitness'])
-
-    # print(len(history_fitness), n_evaluations, pop.champion.fitness)
 
     history_fitness = np.array(history_fitness)
 
@@ -108,8 +106,8 @@ def evo_regression():
     graph = gp.CGPGraph(pop.champion.genome)
     sympy_expr = graph.compile_sympy_expression()
     print(graph.pretty_print())
-    print(sympy_expr[0])
-    print(sympy_expr[0].simplify())
+    print('evolved expression:', sympy_expr[0])
+    print('evolved expression (simplified):', sympy_expr[0].simplify())
     # sympy.plot(sympy_expr[0])
 
     # export computational graphs; create pdf with
