9e2de7919bae7c6f3dbd9fbd8167909b08d052b2
Jakob Jordan <jakobjordan@posteo.de>
2020-03-23 22:22:58 +0100

    Add tests for CGPDiv, CGPPow

--- test/test_cgp_node.py
+++ test/test_cgp_node.py
@@ -69,6 +69,48 @@ def test_mul():
     assert abs((x[0] * x[1]) - y[0]) < 1e-15
 
 
+def test_div():
+    params = {"n_inputs": 2, "n_outputs": 1, "n_columns": 1, "n_rows": 1, "levels_back": 1}
+
+    primitives = [gp.CGPDiv]
+    genome = gp.CGPGenome(
+        params["n_inputs"],
+        params["n_outputs"],
+        params["n_columns"],
+        params["n_rows"],
+        params["levels_back"],
+        primitives,
+    )
+    genome.dna = [-1, None, None, -1, None, None, 0, 0, 1, -2, 2, None]
+    graph = gp.CGPGraph(genome)
+
+    x = [5.0, 1.5]
+    y = graph(x)
+
+    assert abs((x[0] / x[1]) - y[0]) < 1e-15
+
+
+def test_pow():
+    params = {"n_inputs": 2, "n_outputs": 1, "n_columns": 1, "n_rows": 1, "levels_back": 1}
+
+    primitives = [gp.CGPPow]
+    genome = gp.CGPGenome(
+        params["n_inputs"],
+        params["n_outputs"],
+        params["n_columns"],
+        params["n_rows"],
+        params["levels_back"],
+        primitives,
+    )
+    genome.dna = [-1, None, None, -1, None, None, 0, 0, 1, -2, 2, None]
+    graph = gp.CGPGraph(genome)
+
+    x = [5.0, 1.5]
+    y = graph(x)
+
+    assert abs((x[0] ** x[1]) - y[0]) < 1e-15
+
+
 def test_const_float():
     params = {"n_inputs": 2, "n_outputs": 1, "n_columns": 1, "n_rows": 1, "levels_back": 1}
 
