39705537908751c9e6971c008d0cb9696d720a32
Jakob Jordan <jakobjordan@posteo.de>
2020-05-06 19:37:22 +0200

    Add test for gradient-based local search

---
+++ test/test_local_search.py
@@ -0,0 +1,33 @@
+import pytest
+
+import gp
+
+
+def test_gradient_based_step_towards_maximum():
+    torch = pytest.importorskip("torch")
+
+    primitives = [gp.Parameter]
+    genome = gp.Genome(1, 1, 1, 1, 1, primitives)
+    # f(x) = c
+    genome.dna = [-1, None, 0, 0, -2, 1]
+    ind = gp.individual.Individual(None, genome)
+
+    def objective(f):
+        x_dummy = torch.zeros((1, 1), dtype=torch.double)  # not used
+        target_value = torch.ones((1, 1), dtype=torch.double)
+        return torch.nn.MSELoss()(f(x_dummy), target_value)
+
+    # test increase parameter value if too small
+    ind.parameter_names_to_values["<p1>"] = 0.9
+    gp.local_search.gradient_based([ind], objective, 0.05, 1)
+    assert ind.parameter_names_to_values["<p1>"] == pytest.approx(0.91)
+
+    # test decrease parameter value if too large
+    ind.parameter_names_to_values["<p1>"] = 1.1
+    gp.local_search.gradient_based([ind], objective, 0.05, 1)
+    assert ind.parameter_names_to_values["<p1>"] == pytest.approx(1.09)
+
+    # test no change of parameter value if at optimum
+    ind.parameter_names_to_values["<p1>"] = 1.0
+    gp.local_search.gradient_based([ind], objective, 0.05, 1)
+    assert ind.parameter_names_to_values["<p1>"] == pytest.approx(1.0)
