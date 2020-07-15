f2c94b2733001dc099f194507637fb4c27c97b0c
Jakob Jordan <jakobjordan@posteo.de>
2020-07-15 15:53:21 +0200

    Improve docs for nodes

--- cgp/node_impl.py
+++ cgp/node_impl.py
@@ -2,6 +2,8 @@ from .node import OperatorNode
 
 
 class ConstantFloat(OperatorNode):
+    """A node with a constant output."""
+
     _arity = 0
     _def_output = "1.0"
     _def_numpy_output = "np.ones(x.shape[0]) * 1.0"
@@ -9,32 +11,49 @@ class ConstantFloat(OperatorNode):
 
 
 class Add(OperatorNode):
+    """A node that adds its two inputs."""
+
     _arity = 2
     _def_output = "x_0 + x_1"
 
 
 class Sub(OperatorNode):
+    """A node that substracts its second from its first input."""
+
     _arity = 2
     _def_output = "x_0 - x_1"
 
 
 class Mul(OperatorNode):
+    """A node that multiplies its two inputs."""
+
     _arity = 2
     _def_output = "x_0 * x_1"
 
 
 class Div(OperatorNode):
+    """A node that devides its first by its second input."""
+
     _arity = 2
     _def_output = "x_0 / x_1"
 
 
 class Pow(OperatorNode):
+    """A node that raises its first to the power of its second input."""
+
     _arity = 2
     _def_output = "x_0 ** x_1"
     _def_numpy_output = "np.power(x_0, x_1)"
 
 
 class Parameter(OperatorNode):
+    """A node that provides a parametrized constant output.
+
+    The value of the parameter can be adapted via local search and is
+    passed on from parents to their offspring.
+
+    """
+
     _arity = 0
     _initial_values = {"<p>": lambda: 1.0}
     _def_output = "<p>"
--- docs/api_reference/nodes.rst
+++ docs/api_reference/nodes.rst
@@ -1,7 +1,30 @@
-Functional nodes
-================
+Nodes
+=====
+
+==============		   
+Abstract Nodes
+==============
 
 .. automodule:: cgp.node
    :members: 
    :undoc-members:
    :show-inheritance:
+
+==================
+Input/Output Nodes
+==================
+
+.. automodule:: cgp.node_input_output
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+==============
+Operator Nodes
+==============
+
+.. automodule:: cgp.node_impl
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
