5af4189c8ec505fd40149c7d54908e88a463cc9b
Jakob Jordan <jakobjordan@posteo.de>
2020-07-27 21:25:38 +0200

    Add test to check that using cache decorator with fec results in identical history

--- test/test_utils.py
+++ test/test_utils.py
@@ -61,9 +61,69 @@ def test_cache_decorator_produces_identical_history(
         def recording_callback(pop):
             history["fitness_champion"].append(pop.champion.fitness)
 
-        cgp.evolve(
-            pop, objective, ea, **evolve_params, print_progress=True, callback=recording_callback
-        )
+        cgp.evolve(pop, objective, ea, **evolve_params, callback=recording_callback)
+
+        return history
+
+    history = evolve(inner_objective)
+    history_decorated = evolve(inner_objective_decorated)
+
+    for fitness, fitness_decorated in zip(
+        history["fitness_champion"], history_decorated["fitness_champion"]
+    ):
+        assert fitness == pytest.approx(fitness_decorated)
+
+
+@pytest.mark.parametrize("individual_type", ["SingleGenome", "MultiGenome"])
+def test_fec_cache_decorator_produces_identical_history(
+    individual_type, rng_seed, population_params, genome_params, ea_params
+):
+
+    evolve_params = {"max_generations": 10, "min_fitness": 0.0}
+
+    def f_target(x):
+        return x[0] - x[1]
+
+    def inner_objective(ind):
+        np.random.seed(rng_seed)
+
+        if individual_type == "SingleGenome":
+            f = ind.to_func()
+        elif individual_type == "MultiGenome":
+            f = ind.to_func()[0]
+        else:
+            raise NotImplementedError
+
+        loss = 0
+        for x in np.random.uniform(size=(5, 2)):
+            loss += (f_target(x) - f(x)[0]) ** 2
+        return loss
+
+    @cgp.utils.disk_cache(tempfile.mkstemp()[1], use_fec=True, fec_seed=rng_seed)
+    def inner_objective_decorated(ind):
+        return inner_objective(ind)
+
+    def evolve(inner_objective):
+        def objective(ind):
+            ind.fitness = -inner_objective(ind)
+            return ind
+
+        if individual_type == "SingleGenome":
+            pop = cgp.Population(**population_params, genome_params=genome_params)
+        elif individual_type == "MultiGenome":
+            pop = cgp.Population(**population_params, genome_params=[genome_params])
+        else:
+            raise NotImplementedError
+
+        ea = cgp.ea.MuPlusLambda(**ea_params)
+
+        history = {}
+        history["fitness_champion"] = []
+
+        def recording_callback(pop):
+            history["fitness_champion"].append(pop.champion.fitness)
+
+        cgp.evolve(pop, objective, ea, **evolve_params, callback=recording_callback)
 
         return history
 
