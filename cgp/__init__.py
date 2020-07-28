17adfc295d22b47b25c27072265293cf1c0fe089
Maximilian Schmidt <schmidt.maximilian@posteo.de>
2020-07-28 22:26:32 +0900

    Move metadata into __init__.py
    Read metadata in setup.py from __init__.py
    Remove __version__.py
    Read metadata in docs from package
    Bump version to 0.2.0dev
    
    Co-authored-by: Jakob Jordan <1562742+jakobj@users.noreply.github.com>

--- cgp/__init__.py
+++ cgp/__init__.py
@@ -1,16 +1,15 @@
-from .__version__ import __version__
+__version__ = "0.2.0dev"
+__maintainer__ = "Jakob Jordan, Maximilian Schmidt"
+__author__ = "Happy Algorithms League"
+__license__ = "GPLv3"
+__description__ = "Cartesian Genetic Programming in pure Python."
+__url__ = "https://happy-algorithms-league.github.io/hal-cgp/"
+__doc__ = f"{__description__} <{__url__}>"
+
 from .genome import Genome
 from .cartesian_graph import CartesianGraph
 from .node import OperatorNode
-from .node_impl import (
-    Add,
-    ConstantFloat,
-    Div,
-    Mul,
-    Parameter,
-    Pow,
-    Sub,
-)
+from .node_impl import Add, ConstantFloat, Div, Mul, Parameter, Pow, Sub
 from .population import Population
 
 from .hl_api import evolve
